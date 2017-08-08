edbbbcf7d9f2700df3a31c0553af05f810aa9ae4
Azai Chan <azai.tw@gmail.com>
2017-08-09 01:43:18 +0800

    Ignore dist folder, remove post reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -5,13 +5,11 @@ import event from '../ducks/event'
 import manager from '../ducks/manager'
 import racer from '../ducks/racer'
 import team from '../ducks/team'
-import posts from '../ducks/posts'
 
 export default combineReducers({
   account,
   event,
   manager,
-  posts,
   racer,
   team
 })
